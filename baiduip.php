1f1285068445417077a09a719ca8f682576cf928
ausdruck <maasdruck@gmail.com>
2015-04-21 18:42:04 +0800

    百度的IP地址是多少

---
+++ baiduip.php
@@ -0,0 +1,168 @@
+<?php header ('Cache-Control: max-age=2592000');?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-cmn-Hans" xml:lang="zh-cmn-Hans">
+<head>
+<meta name="renderer" content="webkit" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0,minimal-ui" />
+<meta name="apple-mobile-web-app-title" content="百度的IP地址是多少" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<meta name="format-detection" content="telephone=no" />
+<meta content="text/html;charset=UTF-8" http-equiv="Content-Type" />
+<title>百度的IP地址是多少</title>
+<meta content="百度,ip,地址,地理位置,运营商" name="keywords" />
+<meta content="百度的IP地址是多少？" name="description" />
+<style type="text/css">
+body,div,h1{
+margin:0;
+}
+body{
+color:#666;
+background-color:#f8f7f5;
+font-family:"Hiragino Sans GB","Microsoft Yahei","微软雅黑","Helvetica Neue",Helvetica,Tohoma,Arial,Verdana,sans-serif;
+height:100%;
+}
+table{
+width:100%;
+border-collapse:collapse;
+border:0.0625em solid #390BDE;
+margin-bottom:1.75em;
+}
+thead{
+}
+th,td{
+border:0.0625em solid;
+padding:0.1875em;
+}
+.bold{
+font-size:1em;
+font-weight:bold;
+word-break:normal;
+word-wrap:break-word
+}
+.headbold{
+font-size:1.125em;
+font-weight:bold;
+word-break:normal;
+word-wrap:break-word
+}
+.detail{
+width:17.375em;
+margin:0 auto;
+padding:1.25em;
+padding-top:0;
+border-left:.0625em solid #ccc;
+border-bottom:.0625em solid #ccc;
+border-right:.0625em solid #ccc;
+background-color:#ffffff;
+}
+.header{
+padding-top:1.25em;
+padding-bottom:.625em;
+border-bottom:.0625em dotted #ccc
+}
+.center{
+text-align:center;
+}
+a {
+color:#607FA6;
+font-size:1em;
+text-decoration:none;
+}
+</style>
+</head>
+<body>
+<div class="detail">
+<?php
+echo '<div class="header"><h1 class="bold">百度的IP地址是多少</h1></div>';
+$ip = array
+    (
+    array('58.217.200.13', '江苏省&nbsp;南京市', '电信'),
+    array('58.217.200.15', '江苏省&nbsp;南京市', '电信'),
+    array('58.217.200.37', '江苏省&nbsp;南京市', '电信'),
+    array('58.217.200.39', '江苏省&nbsp;南京市', '电信'),
+    array('61.135.185.31', '北京市', '联通'),
+    array('61.135.185.32', '北京市', '联通'),
+    array('61.135.169.103', '北京市', '联通'),
+    array('61.135.169.105', '北京市', '联通'),
+    array('61.135.169.107', '北京市', '联通'),
+    array('61.135.169.113', '北京市', '联通'),
+    array('61.135.169.114', '北京市', '联通'),
+    array('61.135.169.115', '北京市', '联通'),
+    array('61.135.169.121', '北京市', '联通'),
+    array('61.135.169.125', '北京市', '联通'),
+    array('111.13.12.139', '北京市', '移动'),
+    array('111.13.12.142', '北京市', '移动'),
+    array('111.13.100.91', '北京市', '移动'),
+    array('111.13.100.92', '北京市', '移动'),
+    array('115.239.210.25', '浙江省&nbsp;杭州市', '电信'),
+    array('115.239.210.26', '浙江省&nbsp;杭州市', '电信'),
+    array('115.239.210.27', '浙江省&nbsp;杭州市', '电信'),
+    array('115.239.210.28', '浙江省&nbsp;杭州市', '电信'),
+    array('115.239.211.109', '浙江省&nbsp;杭州市', '电信'),
+    array('115.239.211.112', '浙江省&nbsp;杭州市', '电信'),
+    array('115.239.211.113', '浙江省&nbsp;杭州市', '电信'),
+    array('115.239.211.114', '浙江省&nbsp;杭州市', '电信'),
+    array('119.75.213.50', '北京市', '联通'),
+    array('119.75.213.51', '北京市', '联通'),
+    array('119.75.213.61', '北京市', '联通'),
+    array('119.75.216.20', '北京市', '联通'),
+    array('119.75.217.11', '北京市', '联通'),
+    array('119.75.217.26', '北京市', '联通'),
+    array('119.75.217.56', '北京市', '联通'),
+    array('119.75.217.63', '北京市', '联通'),
+    array('119.75.217.109', '北京市', '联通'),
+    array('119.75.218.11', '北京市', '联通'),
+    array('119.75.218.45', '北京市', '联通'),
+    array('119.75.218.70', '北京市', '联通'),
+    array('119.75.218.77', '北京市', '联通'),
+    array('119.75.218.143', '北京市', '联通'),
+    array('123.125.65.78', '北京市', '联通'),
+    array('123.125.65.82', '北京市', '联通'),
+    array('123.125.65.88', '北京市', '联通'),
+    array('123.125.65.90', '北京市', '联通'),
+    array('123.125.114.107', '北京市', '联通'),
+    array('123.125.114.220', '北京市', '联通'),
+    array('123.125.114.238', '北京市', '联通'),
+    array('123.125.115.140', '北京市', '联通'),
+    array('123.125.115.165', '北京市', '联通'),
+    array('180.76.2.183', '北京市', '联通'),
+    array('180.76.3.151', '北京市', '联通'),
+    array('180.97.33.67', '江苏省&nbsp;泰州市', '电信'),
+    array('180.97.33.71', '江苏省&nbsp;泰州市', '电信'),
+    array('180.97.33.107', '江苏省&nbsp;泰州市', '电信'),
+    array('180.97.33.108', '江苏省&nbsp;泰州市', '电信'),
+    array('180.149.131.98', '北京市', '电信'),
+    array('180.149.132.151', '北京市', '电信'),
+    array('180.149.132.166', '北京市', '电信'),
+    array('180.149.132.168', '北京市', '电信'),
+    array('202.108.22.5', '北京市', '联通'),
+    array('202.108.22.142', '北京市', '联通'),
+    array('220.181.6.6', '北京市', '电信'),
+    array('220.181.6.18', '北京市', '电信'),
+    array('220.181.6.19', '北京市', '电信'),
+    array('220.181.6.175', '北京市', '电信'),
+    array('220.181.37.55', '北京市', '电信'),
+    array('220.181.111.22', '北京市', '电信'),
+    array('220.181.111.37', '北京市', '电信'),
+    array('220.181.111.83', '北京市', '电信'),
+    array('220.181.111.111', '北京市', '电信'),
+    array('220.181.111.149', '北京市', '电信'),
+    array('220.181.111.188', '北京市', '电信'),
+    array('220.181.112.12', '北京市', '电信'),
+    array('220.181.112.18', '北京市', '电信'),
+    array('220.181.112.21', '北京市', '电信'),
+    array('220.181.112.76', '北京市', '电信'),
+    array('220.181.112.89', '北京市', '电信'),
+    array('220.181.112.147', '北京市', '电信'),
+    array('220.181.112.195', '北京市', '电信'),
+    array('220.181.112.244', '北京市', '电信')
+    );
+echo '<h2 class="bold">百度 ip 地址，地理位置，运营商</h2>';
+    foreach ($ip as $k => $v) {
+    echo '<p><a href="http://'.$ip[$k][0].'" rel="external nofollow noreferrer" target="_blank">'.$ip[$k][0].'</a>&nbsp;'.$ip[$k][1].'&nbsp;'.$ip[$k][2].'</p>';
+    };?>
+</div>
+</body>
+</html>
