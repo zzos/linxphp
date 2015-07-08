c7a174950ee03a56fc1ca18ffc4317afa47cebb5
maas wu <maasdruck@gmail.com>
2015-07-08 18:12:00 +0800

    百度搜索结果参数分析工具1.16 自动生成标题升级到 v2.3 删除失效的右侧相关搜索{20558) 增加缓存功能，减少对百度服务器的请求数量

--- baidu-f.php
+++ baidu-f.php
@@ -12,7 +12,7 @@
 <meta content="text/html;charset=UTF-8" http-equiv="Content-Type" />
 <?php
 
-// 自动生成标题 v2.2
+// 自动生成标题 v2.3
 
 // 请手动修改 url 对应网址、标题后缀
 $url = 'http://'.$_SERVER['HTTP_HOST'].'/baidu-f.php';
@@ -46,15 +46,87 @@ if (strlen($s) > 0) {
     );
     $z = preg_replace($p[1], $r[1], $s);
     $query = htmlspecialchars(preg_replace($p, $r, $s));
-
-    // 右侧相关搜索 第 1 位查询扩展作为主标题
-    $srcid = json_decode(file_get_contents('http://opendata.baidu.com/api.php?resource_id=20558&format=json&ie=utf-8&oe=utf-8&query='.$query), true);
-    if (preg_match('/(?<=20558\])(.+)(?=\[\/url\])/', @$srcid['data'][0]['tips'][0]['label1'], $w));
 }
 echo '<title>';
 if (strlen($s) > 0) {
-    if (strlen(@$w[1]) > 0) {
-        echo $w[1].'_';
+
+    // 缓存
+    class FCache {
+        public $cache_path = 'stock/';
+        public $cache_time = 86400;
+        public $cache_extension = '.stk';
+    
+        public function __construct($cache_path = 'stock/', $cache_time = 86400, $cache_exttension = '.stk') {
+            $this->cache_path = $cache_path;
+            $this->cache_time = $cache_time;
+            $this->cache_exttension = $cache_exttension;
+            if (!file_exists($this->cache_path)) {
+                mkdir($this->cache_path, 0777);
+            }
+        }
+    
+        public function add($key, $value) {
+            $filename = $this->_get_cache_file($key);
+            file_put_contents($filename, serialize($value), LOCK_EX);
+        }
+    
+        public function delete($key) {
+            $filename = $this->_get_cache_file($key);
+            unlink($filename);
+        }
+    
+        public function get($key) {
+            if ($this->_has_cache($key)) {
+                $filename = $this->_get_cache_file($key);
+                $value = file_get_contents($filename);
+                if (empty($value)) {
+                    return false;
+                }
+                return unserialize($value);
+            }
+        }
+    
+        public function flush() {
+            $fp = opendir($this->cache_path);
+            while(!false == ($fn = readdir($fp))) {
+                if($fn == '.' || $fn =='..') {
+                    continue;
+                }
+                unlink($this->cache_path . $fn);
+            }
+        }
+    
+        private function _has_cache($key) {
+            $filename = $this->_get_cache_file($key);
+            if(file_exists($filename) && (filemtime($filename) + $this->cache_time >= time())) {
+                return true;
+            }
+            return false;
+        }
+    
+        private function _is_valid_key($key) {
+            if ($key != null) {
+                return true;
+            }
+            return false;
+        }
+    
+        private function _safe_filename($key) {
+            if ($this->_is_valid_key($key)) {
+                return md5($key);
+            }
+            return 'unvalid_cache_key';
+        }
+    
+        private function _get_cache_file($key) {
+            return $this->cache_path . $this->_safe_filename($key) . $this->cache_extension;
+        }
+    }
+
+    $cache = new FCache();
+
+    if (strlen($cache->get(urlencode($query))) > 0) {
+        echo $cache->get(urlencode($query)).'_';
     }
     else {
 
@@ -90,6 +162,7 @@ if (strlen($s) > 0) {
 
         if (strlen($sug) > 0) {
             echo $sug.'_';
+            $cache->add(urlencode($query), $sug);
         }
     }
 
@@ -103,15 +176,13 @@ echo $pt."</title>\r\n";
 // 下拉框提示词第 1 位，查询词作为 meta keywords
 echo '<meta content="';
 if (strlen($s) > 0) {
-    if (strlen(@$w[1]) > 0) {
-        echo $w[1].',';
+    if (strlen($cache->get(urlencode($query))) > 0) {
+        echo $cache->get(urlencode($query)).',';
     }
-    else {
-        if (strlen($sug) > 0) {
-            echo $sug.',';
-        }
+    elseif (strlen($sug) > 0) {
+        echo $sug.',';
     }
-echo htmlspecialchars($z, ENT_QUOTES).',';
+    echo htmlspecialchars($z, ENT_QUOTES).',';
 }
 ?>
 百度搜索结果参数,F,F1,F2,F3" name="keywords" />
@@ -10267,7 +10338,7 @@ echo"
 	<a class=\"noa\" href=\"http://top.baidu.com/buzz?b=1\"  target=\"_blank\" rel=\"external nofollow\">百度实时热点排行榜</a>
 	<a class=\"noa\" href=\"http://www.weixingon.com/baidusp-srcid.php\" target=\"_blank\">百度搜索产品资源</a>
 	<a class=\"noa\" href=\"http://www.weixingon.com/baiduip.php\" target=\"_blank\">百度的IP地址是多少</a>
-	<a class=\"noa\" href=\"https://github.com/ausdruck/baidu-prm/blob/master/baidu-f.php\" target=\"_blank\" rel=\"external nofollow\">百度参数分析工具v1.15</a>
+	<a class=\"noa\" href=\"https://github.com/ausdruck/baidu-prm/blob/master/baidu-f.php\" target=\"_blank\" rel=\"external nofollow\">百度参数分析工具v1.16</a>
 </p>
 ";
 }
